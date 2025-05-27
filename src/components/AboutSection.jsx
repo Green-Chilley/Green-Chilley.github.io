00daa708696ca0fc1f08e672eeb743da74bed49b
Leo <leo.nguyen@student.adelaide.edu.au>
2025-05-27 17:17:09 +0930

    fixed resume path

--- src/components/AboutSection.jsx
+++ src/components/AboutSection.jsx
@@ -29,7 +29,7 @@ export const AboutSection = () => {
                                 Get In Touch
                             </a>
                             <a 
-                                href="/public/Resume.pdf"
+                                href="/Resume.pdf"
                                 download="leo_resume"
                                 className="px-6 py-2 rounded-full border border-primary text-primary hover:bg-primary/10 transition-colors duration-300"
                             >
