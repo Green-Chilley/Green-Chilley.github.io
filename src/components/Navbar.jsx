f6d78ebc1b4d595841d0355dcd8e22cb18d7ad7f
Leo <leo.nguyen@student.adelaide.edu.au>
2025-06-18 14:38:47 +0930

    fixed path maybe

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -8,7 +8,7 @@ const navItems = [
     {name: "Skills", href: "#skills"},
     {name: "Projects", href: "#projects"},
     {name: "Contact", href: "#contact"},
-    {name: "Writeups", href: "writeup"},
+    {name: "Writeups", href: "/writeup"},
 ];
 
 export const Navbar = () => {
